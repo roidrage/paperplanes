f1081c567c07386dfd129db6580081e83dd68eed
Mathias Meyer <meyer@paperplanes.de>
2009-02-09 19:21:53 +0100

    Embedded disqus.

--- javascripts/site.js
+++ javascripts/site.js
@@ -22,36 +22,18 @@ function init()
 	tag_block = document.getElementById('tags');
 	author_block = document.getElementById('authors');
 
-	default_field_value = 'Enter your terms, hit enter';
 	message_when_searching = 'Searching...';
-	message_when_done = '';
-	results_when_searching = '';
 
 	passive_search_text_color = '#777';
 	active_search_text_color = '#000';
-
-	isIE = false;
-
-	searchInit(search_field); // capture key events
-	clearSearch(); // set everything right with search field / areas
 }
 
-function clearSearch()
-// clears the search field, sets its default color and value, hides appropriate areas
-{
-  // search_div.style.display = 'none';
-	search_field.style.color = passive_search_text_color;
-	search_field.value = default_field_value;
-	results_wrapper_div.style.display = 'none';
-}
 function searchFocus()
 {
 	search_field.style.color = active_search_text_color;
-	if (search_field.value == default_field_value)
-	{
-		search_field.value = 'site:paperplanes.de ';
-	}
+	search_field.value = 'site:paperplanes.de ';
 }
+
 function showSearch()
 {
 	if (search_div.style.display == 'block')
