e5c84542c0d0b8187fcb12db8ae6b4f309b84f6f
Mathias Meyer <meyer@paperplanes.de>
2011-09-07 23:06:27 +0200

    Remove prototype.js and lightbox.js.
    Not needed anymore, and prototype seems to break disqus.

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,27 +1,9 @@
-/** $Id: site.js 275 2007-01-11 21:06:20Z garrett $ **/
-
-/**
- * simplelog site functions
- * hides appropriate divs on launch, shows divs, etc, etc
- * search box functionality as well
- */
- 
 window.onload = function()
 {
 	init(); // see below, this is to get around global vars not being ready
 }
 function init()
-// init vars, run some initial functions
 {
-	body_container = document.getElementById('wrapper');
-	search_field = document.getElementById('q');
-	search_div = document.getElementById('search');
-	results_wrapper_div = document.getElementById('search_results');
-	loading_msg_span = document.getElementById('loading');
-	results_span = document.getElementById('results');
-	tag_block = document.getElementById('tags');
-	author_block = document.getElementById('authors');
-
 	message_when_searching = 'Searching...';
 
 	passive_search_text_color = '#777';
@@ -34,14 +16,3 @@ function searchFocus()
 	search_field.value = 'site:paperplanes.de ';
 }
 
-function showSearch()
-{
-	if (search_div.style.display == 'block')
-	{
-		clearSearch();
-	}
-	else
-	{
-		search_div.style.display = 'block';
-	}
-}
