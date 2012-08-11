b3323d9ca2d07a67abfa1f253f1e91f155c06aea
Mathias Meyer <meyer@paperplanes.de>
2012-08-11 22:02:33 +0200

    Add generator for tags.

---
+++ _plugins/tags.rb
@@ -0,0 +1,24 @@
+module Paperplanes
+  class TagPage < Jekyll::Page
+    def initialize(site, base, category, posts)
+      @site = site
+      @base = base
+      @dir = 'tags' 
+      @name = "#{category}.html"
+
+      process(@name)
+      read_yaml(File.join(base, '_layouts'), 'tag.html')
+      data['tag'] = category
+      data['posts'] = posts
+      data['title'] = "Tag archive: #{category}"
+    end
+  end
+
+  class Tags < Jekyll::Generator
+    def generate(site)
+      site.tags.each do |tag, posts|
+        site.pages << TagPage.new(site, site.source, tag, posts)
+      end
+    end
+  end
+end
